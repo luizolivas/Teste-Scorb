c35331280a21aa06dda88eb09015668223305787
luizolivas <luizolivas@gmail.com>
2025-07-30 14:31:17 -0300

    feat: cria serviço e contrato faturaItem

---
+++ TesteDengine.Application/Services/FaturaItemService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TesteDengine.Application.Services.DTOs;
+using TesteDengine.Application.Services.Interfaces;
+
+namespace TesteDengine.Application.Services
+{
+    public class FaturaItemService : IFaturaItemService
+    {
+        public Task AddAsync(FaturaItemCreateDTO dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<FaturaItemDTO>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<FaturaItemDTO>> GetAllByFaturaIdAsync(int faturaId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<FaturaItemDTO?> GetByFaturaIdAsync(int faturaId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<FaturaItemDTO?> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(FaturaItemUpdateDTO dto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TesteDengine.Application/Services/Interfaces/IFaturaItemService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TesteDengine.Application.Services.DTOs;
+
+namespace TesteDengine.Application.Services.Interfaces
+{
+    public interface IFaturaItemService
+    {
+        Task<IEnumerable<FaturaItemDTO>> GetAllAsync();
+        Task<IEnumerable<FaturaItemDTO>> GetAllByFaturaIdAsync(int faturaId);
+        Task<FaturaItemDTO?> GetByIdAsync(int id);
+        Task<FaturaItemDTO?> GetByFaturaIdAsync(int faturaId);
+        Task AddAsync(FaturaItemCreateDTO dto);
+        Task UpdateAsync(FaturaItemUpdateDTO dto);
+        Task DeleteAsync(int id);
+    }
+}
