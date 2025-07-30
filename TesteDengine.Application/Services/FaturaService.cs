6d419306f45f50c5c978332b5ed23a738e9c27ae
luizolivas <luizolivas@gmail.com>
2025-07-30 14:31:09 -0300

    feat: cria serviço e contrato fatura

---
+++ TesteDengine.Application/Services/FaturaService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TesteDengine.Application.Services.DTOs;
+using TesteDengine.Application.Services.Interfaces;
+
+namespace TesteDengine.Application.Services
+{
+    public class FaturaService : IFaturaService
+    {
+        public Task AddAsync(FaturaCreateDTO dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<FaturaDTO>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<FaturaDTO>> GetAllByClienteIdAsync(int idCliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<FaturaDTO?> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(FaturaUpdateDTO dto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TesteDengine.Application/Services/Interfaces/IFaturaService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TesteDengine.Application.Services.DTOs;
+
+namespace TesteDengine.Application.Services.Interfaces
+{
+    public interface IFaturaService
+    {
+        Task<IEnumerable<FaturaDTO>> GetAllAsync();
+        Task<IEnumerable<FaturaDTO>> GetAllByClienteIdAsync(int idCliente);
+        Task<FaturaDTO?> GetByIdAsync(int id);
+        Task AddAsync(FaturaCreateDTO dto);
+        Task UpdateAsync(FaturaUpdateDTO dto);
+        Task DeleteAsync(int id);
+    }
+}
