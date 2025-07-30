c7ffe631ccfeaac14c5c82a38d9cf0f0c01081d7
luizolivas <luizolivas@gmail.com>
2025-07-30 14:23:35 -0300

    feat: Cria entidades

---
+++ TesteDengine.Domain/Entities/Cliente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteDengine.Domain.Entities
+{
+    public class Cliente
+    {
+        public int ClienteId { get; set; }
+        public string Nome { get; set; } 
+        public virtual ICollection<Fatura> Faturas { get; set; } = new HashSet<Fatura>();
+    }
+}
--- TesteDengine.Domain/Entities/Fatura.cs
+++ TesteDengine.Domain/Entities/Fatura.cs
@@ -1,9 +1,12 @@
-﻿namespace TesteDengine;
+﻿using TesteDengine.Domain.Entities;
+
+namespace TesteDengine;
 
 public class Fatura
 {
     public int FaturaId { get; set; }
-    public string Cliente { get; set; }
+    public int ClienteId { get; set; } 
+    public Cliente Cliente { get; set; } 
     public DateTime Data { get; set; }
 
     public virtual ICollection<FaturaItem> FaturaItem { get; set; }
