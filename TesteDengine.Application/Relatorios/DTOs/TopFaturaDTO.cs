4b98a0bcea86ca59c3191f3d90c97193d1e6e941
luizolivas <luizolivas@gmail.com>
2025-07-30 22:44:33 -0300

    feat: cria dtos para relatorios

---
+++ TesteDengine.Application/Relatorios/DTOs/TopFaturaDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteDengine.Application.Relatorios.DTOs
+{
+    public class TopFaturaDTO
+    {
+        public int FaturaId { get; set; }
+        public string ClienteNome { get; set; } 
+        public double Total { get; set; }
+    }
+}
---
+++ TesteDengine.Application/Relatorios/DTOs/TotalPorClienteDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteDengine.Application.Relatorios.DTOs
+{
+    public class TotalPorClienteDTO
+    {
+        public int ClienteId { get; set; } 
+        public string ClienteNome { get; set; } 
+        public double Total { get; set; }
+    }
+}
